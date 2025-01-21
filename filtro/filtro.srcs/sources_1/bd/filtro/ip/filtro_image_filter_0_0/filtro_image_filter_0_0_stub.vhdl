d069ec61af3b9258a96c7aef5165f18da136bc6a
Estrellabg <137991417+Estrellabg@users.noreply.github.com>
2025-01-21 18:58:08 +0100

    Add files via upload

---
+++ filtro/filtro.srcs/sources_1/bd/filtro/ip/filtro_image_filter_0_0/filtro_image_filter_0_0_stub.vhdl
@@ -0,0 +1,53 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+-- Date        : Mon Jan 20 16:03:23 2025
+-- Host        : PC_Aidamgl running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub -rename_top filtro_image_filter_0_0 -prefix
+--               filtro_image_filter_0_0_ filtro_image_filter_0_0_stub.vhdl
+-- Design      : filtro_image_filter_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity filtro_image_filter_0_0 is
+  Port ( 
+    INPUT_STREAM_TVALID : in STD_LOGIC;
+    INPUT_STREAM_TREADY : out STD_LOGIC;
+    INPUT_STREAM_TDATA : in STD_LOGIC_VECTOR ( 23 downto 0 );
+    INPUT_STREAM_TKEEP : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    INPUT_STREAM_TSTRB : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    INPUT_STREAM_TUSER : in STD_LOGIC_VECTOR ( 0 to 0 );
+    INPUT_STREAM_TLAST : in STD_LOGIC_VECTOR ( 0 to 0 );
+    INPUT_STREAM_TID : in STD_LOGIC_VECTOR ( 0 to 0 );
+    INPUT_STREAM_TDEST : in STD_LOGIC_VECTOR ( 0 to 0 );
+    OUTPUT_STREAM_TVALID : out STD_LOGIC;
+    OUTPUT_STREAM_TREADY : in STD_LOGIC;
+    OUTPUT_STREAM_TDATA : out STD_LOGIC_VECTOR ( 23 downto 0 );
+    OUTPUT_STREAM_TKEEP : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    OUTPUT_STREAM_TSTRB : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    OUTPUT_STREAM_TUSER : out STD_LOGIC_VECTOR ( 0 to 0 );
+    OUTPUT_STREAM_TLAST : out STD_LOGIC_VECTOR ( 0 to 0 );
+    OUTPUT_STREAM_TID : out STD_LOGIC_VECTOR ( 0 to 0 );
+    OUTPUT_STREAM_TDEST : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ap_clk : in STD_LOGIC;
+    ap_rst_n : in STD_LOGIC;
+    ap_start : in STD_LOGIC;
+    ap_done : out STD_LOGIC;
+    ap_ready : out STD_LOGIC;
+    ap_idle : out STD_LOGIC
+  );
+
+end filtro_image_filter_0_0;
+
+architecture stub of filtro_image_filter_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "INPUT_STREAM_TVALID,INPUT_STREAM_TREADY,INPUT_STREAM_TDATA[23:0],INPUT_STREAM_TKEEP[2:0],INPUT_STREAM_TSTRB[2:0],INPUT_STREAM_TUSER[0:0],INPUT_STREAM_TLAST[0:0],INPUT_STREAM_TID[0:0],INPUT_STREAM_TDEST[0:0],OUTPUT_STREAM_TVALID,OUTPUT_STREAM_TREADY,OUTPUT_STREAM_TDATA[23:0],OUTPUT_STREAM_TKEEP[2:0],OUTPUT_STREAM_TSTRB[2:0],OUTPUT_STREAM_TUSER[0:0],OUTPUT_STREAM_TLAST[0:0],OUTPUT_STREAM_TID[0:0],OUTPUT_STREAM_TDEST[0:0],ap_clk,ap_rst_n,ap_start,ap_done,ap_ready,ap_idle";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "image_filter,Vivado 2018.3";
+begin
+end;
